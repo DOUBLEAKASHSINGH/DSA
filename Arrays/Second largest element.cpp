7e6f2a059ecf0d94db6f2a1c75b322f08992d519
AKASH SINGH <91905043+DOUBLEAKASHSINGH@users.noreply.github.com>
2025-07-22 04:42:15 +0530

    Update Second largest element.cpp

--- Arrays/Second largest element.cpp
+++ Arrays/Second largest element.cpp
@@ -3,6 +3,23 @@
 #include <algorithm>
 using namespace std;
 
+int secondLargest(const vector<int> arr){
+    if(arr.size() < 2) {
+        throw invalid_argument("Array must contain at least two elements.");
+    }
+    return 0;
+}
+
 int main() {
+    int n;
+    vector<int> arr;
+    cout << "Enter the number of elements: ";
     
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+    int secondLargestValue = secondLargest(arr);
+    cout << secondLargestValue << endl;
 }
