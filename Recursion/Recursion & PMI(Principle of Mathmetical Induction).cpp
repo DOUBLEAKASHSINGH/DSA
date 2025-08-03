a25ad4cdc60225d62f41036148914bace4365d33
AKASH SINGH <91905043+DOUBLEAKASHSINGH@users.noreply.github.com>
2025-08-03 23:37:07 +0530

    Add files via upload

---
+++ Recursion/Recursion & PMI(Principle of Mathmetical Induction).cpp
@@ -0,0 +1,10 @@
+/*
+    * Recursion and Principle of Mathematical Induction (PMI) in C++
+    * This code demonstrates the use of recursion and PMI to solve problems.
+
+    -------------------------------------------------------------------------
+    * Base: Prove F(0) or F(1) is true.
+    * Inductive Hypothesis: Assume F(k) is true,
+    * Inductive Step: Using Induction Hypothesis Prove F(k+1) is true.
+
+*/
